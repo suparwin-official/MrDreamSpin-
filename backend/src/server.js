f2e4ad4d83e17ae3550eb534652a11fec7cc8173
suparwin-official <gorommasalahitbd@gmail.com>
2026-05-01 00:57:06 +0600

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,9 @@
+import http from "http";
+import { initSocket } from "./socket/socket.js";
 
+const server = http.createServer(app);
+initSocket(server);
+
+server.listen(PORT, () => {
+  console.log(`🔥 Server + Socket running on ${PORT}`);
+});
