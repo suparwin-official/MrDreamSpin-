681861b59ee792ef5c8739ea305d8affe4a69747
suparwin-official <gorommasalahitbd@gmail.com>
2026-05-01 00:53:23 +0600

    Update slot.logic.js

--- backend/src/utils/slot.logic.js
+++ backend/src/utils/slot.logic.js
@@ -1,1 +1,15 @@
+export const spinSlot = (betAmount) => {
+  const RTP = 0.92; // 92%
+  const rand = Math.random();
 
+  let win = 0;
+
+  if (rand < RTP / 10) win = betAmount * 10;
+  else if (rand < RTP / 4) win = betAmount * 4;
+  else if (rand < RTP / 2) win = betAmount * 2;
+
+  return {
+    win,
+    isWin: win > 0
+  };
+};
