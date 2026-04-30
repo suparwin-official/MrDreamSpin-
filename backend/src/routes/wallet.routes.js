54ab0bec724b87c5922330c5f2851e8a3a4ecd55
suparwin-official <gorommasalahitbd@gmail.com>
2026-05-01 00:50:44 +0600

    Update wallet.routes.js

--- backend/src/routes/wallet.routes.js
+++ backend/src/routes/wallet.routes.js
@@ -1,1 +1,19 @@
+import express from "express";
+import { protect } from "../middleware/auth.middleware.js";
+import {
+  getWallet,
+  deposit,
+  bet,
+  win,
+  history
+} from "../controllers/wallet.controller.js";
 
+const router = express.Router();
+
+router.get("/", protect, getWallet);
+router.post("/deposit", protect, deposit);
+router.post("/bet", protect, bet);
+router.post("/win", protect, win);
+router.get("/history", protect, history);
+
+export default router;
