29283955ec327a2218551e3ff74db2b5d5be7114
suparwin-official <gorommasalahitbd@gmail.com>
2026-05-01 00:49:29 +0600

    Update Transaction.js

--- backend/src/models/Transaction.js
+++ backend/src/models/Transaction.js
@@ -1,1 +1,13 @@
+import mongoose from "mongoose";
 
+const transactionSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    type: { type: String, enum: ["deposit", "bet", "win"], required: true },
+    amount: { type: Number, required: true },
+    balanceAfter: { type: Number, required: true }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Transaction", transactionSchema);
