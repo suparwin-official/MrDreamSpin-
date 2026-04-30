626b05eb9cd8350a1053478d84523bfe16196722
suparwin-official <gorommasalahitbd@gmail.com>
2026-05-01 00:56:07 +0600

    Update socket.js

--- backend/src/socket/socket.js
+++ backend/src/socket/socket.js
@@ -1,1 +1,19 @@
+import { Server } from "socket.io";
 
+export const initSocket = (server) => {
+  const io = new Server(server, {
+    cors: { origin: "*" }
+  });
+
+  io.on("connection", (socket) => {
+    console.log("🟢 User connected:", socket.id);
+
+    socket.on("spin", (data) => {
+      io.emit("spinResult", data);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("🔴 User disconnected");
+    });
+  });
+};
